c16622bedce1ed7927ef9f3e95590084235854fa
djayakody <dilshan.jayakody@xunison.com>
2023-03-25 21:56:42 +0530

    Add initial version of API documentation.

---
+++ doc/html/search/defines_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['returntohome_74',['returnToHome',['../unilcd_8h.html#ae8efa4ddae811bae3b3da9082639dfee',1,'unilcd.h']]]
+];
---
+++ doxygen/generate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ../firmware/lib
+doxygen ../../doxygen/yali-doc
+
